f20f2451459d4e2573aedc30bb8de9deb9555f0d
cloudops-toolkit <cloudopstoolkit@gmail.com>
2025-01-23 14:18:44 +0530

    Updated CORS Settings

--- sports-platform/website/next.config.js
+++ sports-platform/website/next.config.js
@@ -4,7 +4,8 @@ module.exports = {
     return [
       {
         source: '/api/:path*',
-        destination: `${process.env.NEXT_PUBLIC_API_URL}/:path*`
+        destination: `http://sports-platform-alb-54461425.us-east-1.elb.amazonaws.com/api/:path*`
+        // destination: `${process.env.NEXT_PUBLIC_API_URL}/:path*`
       }
     ]
   }
