5b33fc82fed509e3edcf86cce8e794146843ee57
cloudops-toolkit <cloudopstoolkit@gmail.com>
2025-01-23 13:22:27 +0530

    Updated CORS Settings

--- sports-platform/api/SportsApi/Program.cs
+++ sports-platform/api/SportsApi/Program.cs
@@ -11,31 +11,40 @@ builder.Services.AddSwaggerGen(c =>
     c.SwaggerDoc("v1", new OpenApiInfo { Title = "Sports API", Version = "v1" });
 });
 builder.Services.AddScoped<ISportService, SportService>();
-// builder.Services.AddCors(options =>
-// {
-//     options.AddPolicy("AllowAll", builder =>
-//         builder.AllowAnyOrigin()
-//                .AllowAnyMethod()
-//                .AllowAnyHeader());
-// });
 
 builder.Services.AddCors(options =>
 {
     options.AddPolicy("AllowAll", builder =>
-    {
-        builder.WithOrigins(allowedOrigins)
+        builder.AllowAnyOrigin()
                .AllowAnyMethod()
-               .AllowAnyHeader()
-               .AllowCredentials();
-    });
+               .AllowAnyHeader());
 });
+
+// // builder.Services.AddCors(options =>
+// // {
+// //     options.AddPolicy("AllowAll", builder =>
+// //         builder.AllowAnyOrigin()
+// //                .AllowAnyMethod()
+// //                .AllowAnyHeader());
+// // });
+
+// builder.Services.AddCors(options =>
+// {
+//     options.AddPolicy("AllowAll", builder =>
+//     {
+//         builder.WithOrigins(allowedOrigins)
+//                .AllowAnyMethod()
+//                .AllowAnyHeader()
+//                .AllowCredentials();
+//     });
+// });
 
 var app = builder.Build();
 
 app.UseSwagger();
 app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "Sports API v1"));
-app.UseHttpsRedirection();
 app.UseCors("AllowAll");
+app.UseHttpsRedirection();
 app.UseAuthorization();
 app.MapControllers();
 
